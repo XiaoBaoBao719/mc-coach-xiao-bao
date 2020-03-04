94ae06f253ef05111167decc2ff89ac978b429ac
Xiao Bao Bao <xiao-bao719@hotmail.com>
2020-03-04 15:36:08 -0800

    Create bio.rkt
    coach bio

---
+++ bio.rkt
@@ -0,0 +1,6 @@
+#lang mc-coach/bio
+
+(bio
+ #:first-name "Xiao-Bao"
+ #:profile-pic (radial-star 8 8 8 'solid 'red)
+ #:coach-story "A while ago, I decided to help Metacoders expand public computer literacy by coaching...")
